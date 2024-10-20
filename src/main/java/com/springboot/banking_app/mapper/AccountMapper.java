a54e5dfebfbeaadd4b5f19effb0e3cb602546da5
SRM1987 <shaanraja.1986@gmail.com>
2024-10-20 15:56:36 +0530

    Changed DTO class to record class.(New Update)

--- src/main/java/com/springboot/banking_app/mapper/AccountMapper.java
+++ src/main/java/com/springboot/banking_app/mapper/AccountMapper.java
@@ -4,15 +4,15 @@ import com.springboot.banking_app.dto.AccountDto;
 import com.springboot.banking_app.entity.Account;
 
 public class AccountMapper {
-    public static Account mapToAccount(AccountDto accountDto){
+    public static Account mapToAccount(AccountDto accountDto) {
         Account account = new Account(
-                accountDto.getId(),
-                accountDto.getAccountHolderName(),
-                accountDto.getBalance());
+                accountDto.id(),
+                accountDto.accountHolderName(),
+                accountDto.balance());
         return account;
     }
 
-    public static AccountDto mapToAccountDto(Account account){
+    public static AccountDto mapToAccountDto(Account account) {
         AccountDto accountDto = new AccountDto(
                 account.getId(),
                 account.getAccountHolderName(),
